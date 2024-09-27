b0bed842d823891ce81a21097e886112fc35b70e
Aditya Khalkar <22510466.dypit@dypvp.edu.in>
2024-09-27 20:46:46 +0530

    Added info

--- vortexa/app/dashboard/page.tsx
+++ vortexa/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import DisasterDashboard from "@/components/Dashboard";
+import DisasterInfo from "@/components/disasterInfo";
 import React from "react";
 
 const Page: React.FC = () => {
   return (
     <div>
       <DisasterDashboard />
+      <DisasterInfo />
     </div>
   );
 };
